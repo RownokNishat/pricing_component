027a5125a3d6986f9776a6bed7621969289f4fe4
RownokNishat <rownokjahan900@gmail.com>
2023-03-09 04:56:12 +0600

    task_completed

--- src/Pages/Pricing_Component/Pricing_Component.js
+++ src/Pages/Pricing_Component/Pricing_Component.js
@@ -9,17 +9,29 @@ const Pricing_Component = () => {
   return (
     <div className="priceComponent_div">
       <h3 className="pricing_header">Our Pricing</h3>
-      <div className="form-control w-52">
+      <div className="form-control w-52  mx-auto mb-16 ">
         <label className="cursor-pointer label">
-          <span className="label-text">Remember me</span>
-          <input
+          <span className="label-text toggle_text">Annually</span>
+          {/* <input
             type="checkbox"
             className="toggle toggle-primary"
             onChange={(e) => {
               setChange(!change);
             }}
             checked={change}
-          />
+          /> */}
+
+          <label class="switch">
+            <input
+              type="checkbox"
+              onChange={(e) => {
+                setChange(!change);
+              }}
+              checked={change}
+            />
+            <span class="slider round"></span>
+          </label>
+          <span className="label-text toggle_text">Monthly</span>
         </label>
       </div>
       {change ? <Monthly_Page></Monthly_Page> : <Annual_Page></Annual_Page>}
